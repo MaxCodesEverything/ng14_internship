93501eddee5391784efbaa9645d8ccc8e68aac37
Maksim Kazhadub <maksim.kazhadub@innowise.com>
2025-08-01 08:37:46 +0200

    feat(calendar/settings): add toggle to highlight today

--- src/app/components/calendar-day/calendar-day.component.ts
+++ src/app/components/calendar-day/calendar-day.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
-import { TodayDirective } from 'src/app/directives/today-directive.directive';
+import { CalendarDirective } from 'src/app/directives/calendar-directive.directive';
 
 @Component({
     standalone: true,
     selector: 'app-calendar-day',
     templateUrl: './calendar-day.component.html',
     styleUrls: ['./calendar-day.component.scss'],
-    imports: [TodayDirective, CommonModule]
+    imports: [CalendarDirective, CommonModule]
 })
 
 export class CalendarDayComponent{
     @Input() day !: number | null;
     @Input() monthIndex !: number;
     @Input() year !: number;
+
+    @Input() highlightToday: boolean = true;
 }
--- src/app/directives/calendar-directive.directive.ts
+++ src/app/directives/calendar-directive.directive.ts
@@ -1,24 +1,29 @@
-import { Directive, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, Input, OnChanges, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[appCalendarDirective]',
     standalone: true
 })
 
-export class CalendarDirective implements OnInit{
-    @Input() appCalendarDirective!: {year: number, month: number, day: number};
+export class CalendarDirective implements OnChanges{
+    @Input() appCalendarDirective!: {year: number, month: number, day: number} | null;
 
     constructor(private el: ElementRef, private renderer: Renderer2){}
 
-    ngOnInit() {
+    ngOnChanges() {
+        this.checkToday();
+    }
+
+    checkToday(){
+         this.renderer.removeClass(this.el.nativeElement, 'todayDate');
+
         const today = new Date();
         if(
-            this.appCalendarDirective.day === today.getDate() &&
-            this.appCalendarDirective.month === today.getMonth() &&
-            this.appCalendarDirective.year === today.getFullYear()
+            this.appCalendarDirective?.day === today.getDate() &&
+            this.appCalendarDirective?.month === today.getMonth() &&
+            this.appCalendarDirective?.year === today.getFullYear()
         ){
             this.renderer.addClass(this.el.nativeElement, 'todayDate')
         }
-    }
-
+    }  
 }
